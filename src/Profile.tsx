99fc94009d6ada3a8300d81ad6bd1abc628959e5
dingwu-collab <dingwu0711@gmail.com>
2026-01-22 09:09:57 +0800

    Delete src directory

--- src/Profile.tsx
+++
@@ -1,6 +0,0 @@
-  import { createRoot } from "react-dom/client";
-  import App from "./app/Profile_App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
